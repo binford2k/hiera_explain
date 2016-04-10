369eca1986effbaa074334b50a86c8c74c5c7477
Ben Ford <ben.ford@puppetlabs.com>
2016-04-10 16:37:16 -0700

    Don't load datasources so often

--- lib/hiera_explain.rb
+++ lib/hiera_explain.rb
@@ -4,11 +4,6 @@ class HieraExplain
     return {} unless File.exists? path
 
     begin
-      require "hiera_explain/datasource/#{backend}"
-    rescue LoadError => e
-    end
-
-    begin
       return HieraExplain::Datasource.send(backend.to_s, path)
     rescue => e
       puts "The #{backend} backend has a malformed datasource at #{path}"
